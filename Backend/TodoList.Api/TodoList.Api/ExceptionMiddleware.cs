3453c5195d2723957fc5038263d6a34cac0f416f
Alina Dreksler <dreksler.alina@gmail.com>
2023-09-05 17:47:06 +1200

    Add exception middleware

---
+++ Backend/TodoList.Api/TodoList.Api/ExceptionMiddleware.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TodoList.Api.Controllers;
+
+namespace TodoList.Api
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<TodoItemsController> _logger;
+
+        public ExceptionMiddleware(RequestDelegate next, ILogger<TodoItemsController> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                httpContext.Response.ContentType = "application/json";
+                httpContext.Response.StatusCode = ex switch
+                {
+                    (NotFoundException) => (int)HttpStatusCode.NotFound,
+                    (ApiException) => (int)HttpStatusCode.BadRequest,
+                    _ => (int)HttpStatusCode.InternalServerError,
+                };
+
+                var response = JsonSerializer.Serialize(new
+                {
+                    httpContext.Response.StatusCode,
+                    ex.Message
+                });
+                await httpContext.Response.WriteAsync(response);
+            }
+        }
+    }
+
+    public class ApiException: Exception { }
+    public class NotFoundException: Exception { }
+}
--- Backend/TodoList.Api/TodoList.Api/Startup.cs
+++ Backend/TodoList.Api/TodoList.Api/Startup.cs
@@ -49,7 +49,7 @@ namespace TodoList.Api
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoList.Api v1"));
             }
-
+            app.UseMiddleware<ExceptionMiddleware>();
             app.UseHttpsRedirection();
 
             app.UseRouting();
